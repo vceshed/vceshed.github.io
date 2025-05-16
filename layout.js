615083ef73c33942801aed4fda741d5ed2f3c28b
vceshed <atarcompanion@gmail.com>
2025-05-17 09:33:07 +1000

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,7 +1,15 @@
-var body = document.body;
-body.innerHTML = `
-    <div class="parent">
-        <h3>hi</h3>
-        <p id="insert">hi2</p>
+document.createElement('page').innerHTML = `
+
+<img id="banner" src="shelfbanner.jpg" alt="banner">
+<img id="bannercover" src="shelfbanner.jpg" alt="bannercover">
+<div id="page">
+<div id="head" class="box"><h1>ㅤHeader</h1></div>
+  <div id="nav" class="box"><b>sidebar</b><hr><a>item 1</a><a>item 2</a><a>item 3</a><a>item 4</a></div>
+  <div id="content" class="box"><b>Content</b><br><p>Test grid-based layout. Responsive for mobile.<br>Try to hold own back foot to clean it but foot reflexively kicks you in face, go into a rage and bite own foot, hard refuse to leave cardboard box am in trouble, roll over, too cute for human to get mad massacre a bird in the living room and then look like the cutest and most innocent animal on the planet or human give me attention meow. Chase ball of string ooooh feather moving feather! yet carrying out surveillance on the neighbour's dog. Be a nyan cat, feel great about it, be annoying 24/7 poop rainbows in litter box all day haha you hold me hooman i scratch and hate dogs show belly eat half my food and ask for more but try to hold own back foot to clean it but foot reflexively kicks you in face, go into a rage and bite own foot, hard. <br><img id="testimage" src="/Jiji.webp"><br> Crash against wall but walk away like nothing happened cat not kitten around slap the dog because cats rule or cat milk copy park pee walk owner escape bored tired cage droppings sick vet vomit destroy house in 5 seconds so walk on keyboard and making sure that fluff gets into the owner's eyes.
+</p>
 </div>
+  <div id="footer" class="box"><p>footer ᓚ₍ ^. .^₎</p></div>
+</div>
+
 `;
+document.body.appendChild(page);
